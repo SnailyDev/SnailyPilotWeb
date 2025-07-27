53fb34807b163d2eb9d928d39bf1b2f5c53db9ae
SnailyPilot <andrei-mihai.lupu@legitimid.eu>
2025-07-27 22:35:03 +0200

    ik another minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 root: "src",
 build: {
+    rollupOptions: {
+      input: {
+        index: './index.html',
+        license: './license.html'
+      },
+    },
     outDir: '../dist'
 }
 })
